005461e92acf2055bb60901c518c10518208e123
David Lohmeyer <vilepickle@gmail.com>
2018-04-12 10:36:07 -0500

    Issue #2942616 by jasonschweb, vilepickle: Address field isn't cloned properly

--- src/Entity/QuickNodeCloneEntityFormBuilder.php
+++ src/Entity/QuickNodeCloneEntityFormBuilder.php
@@ -22,37 +22,16 @@ class QuickNodeCloneEntityFormBuilder extends EntityFormBuilder {
     /** @var \Drupal\node\Entity\Node $new_node */
     $new_node = $original_entity->createDuplicate();
 
-    // Check for paragraph fields which need to be duplicated as well.
+    // Clone all translations of a node.
     foreach ($new_node->getTranslationLanguages() as $langcode => $language) {
+      /** @var \Drupal\node\Entity\Node $translated_node */
       $translated_node = $new_node->getTranslation($langcode);
-
-      foreach ($translated_node->getFieldDefinitions() as $field_definition) {
-        $field_storage_definition = $field_definition->getFieldStorageDefinition();
-        $field_settings = $field_storage_definition->getSettings();
-        $field_name = $field_storage_definition->getName();
-        if (isset($field_settings['target_type']) && $field_settings['target_type'] == "paragraph") {
-
-          // Each paragraph entity will be duplicated, so we won't be editing the same as the parent in every clone.
-          if (!$translated_node->get($field_name)->isEmpty()) {
-            foreach ($translated_node->get($field_name) as $value) {
-              if ($value->entity) {
-                $value->entity = $value->entity->createDuplicate();
-                foreach($value->entity->getFieldDefinitions() as $field_definition) {
-                  $field_storage_definition = $field_definition->getFieldStorageDefinition();
-                  $pfield_settings = $field_storage_definition->getSettings();
-                  $pfield_name = $field_storage_definition->getName();
-                  \Drupal::moduleHandler()->alter('cloned_node_paragraph_field', $value->entity, $pfield_name, $pfield_settings);
-                }
-              }
-            }
-          }
-        }
-        \Drupal::moduleHandler()->alter('cloned_node', $translated_node, $field_name, $field_settings);
-      }
+      $translated_node = $this->cloneParagraphs($translated_node);
+      \Drupal::moduleHandler()->alter('cloned_node', $node, $field_name, $field_settings);
       $prepend_text = "";
-      $qnc_config = \Drupal::config('quick_node_clone.settings');
-      if(!empty($qnc_config->get('text_to_prepend_to_title'))) {
-        $prepend_text = $qnc_config->get('text_to_prepend_to_title') . " ";
+      $config = \Drupal::config('quick_node_clone.settings');
+      if(!empty($config->get('text_to_prepend_to_title'))) {
+        $prepend_text = $config->get('text_to_prepend_to_title') . " ";
       }
       $translated_node->setTitle(t($prepend_text . '@title', ['@title' => $original_entity->getTitle()], ['langcode' => $langcode]));
     }
@@ -64,7 +43,52 @@ class QuickNodeCloneEntityFormBuilder extends EntityFormBuilder {
     $form_object->setEntity($new_node);
 
     $form_state = (new FormState())->setFormState($form_state_additions);
-    return $this->formBuilder->buildForm($form_object, $form_state);
+    $new_form = $this->formBuilder->buildForm($form_object, $form_state);
+
+    // If we are cloning addresses, we need to reset our delta counter
+    // once the form is built.
+    $tempstore = \Drupal::service('user.private_tempstore')->get('quick_node_clone');
+    if ($tempstore->get('address_initial_value_delta') != NULL) {
+      $tempstore->set('address_initial_value_delta', NULL);
+    }
+
+    return $new_form;
+  }
+
+  /**
+   * Clone the paragraphs of a designated node. If we do not clone the
+   * paragraphs attached to the node, the linked paragraphs will be linked
+   * to two nodes which is not ideal.
+   *
+   * @param \Drupal\node\Entity\Node $node
+   *   The node to clone.
+   *
+   * @return \Drupal\node\Entity\Node
+   *   The node with cloned paragraph fields.
+   */
+  public function cloneParagraphs($node) {
+    foreach ($node->getFieldDefinitions() as $field_definition) {
+      $field_storage_definition = $field_definition->getFieldStorageDefinition();
+      $field_settings = $field_storage_definition->getSettings();
+      $field_name = $field_storage_definition->getName();
+      if (isset($field_settings['target_type']) && $field_settings['target_type'] == "paragraph") {
+
+        if (!$node->get($field_name)->isEmpty()) {
+          foreach ($node->get($field_name) as $value) {
+            if ($value->entity) {
+              $value->entity = $value->entity->createDuplicate();
+              foreach($value->entity->getFieldDefinitions() as $field_definition) {
+                $field_storage_definition = $field_definition->getFieldStorageDefinition();
+                $pfield_settings = $field_storage_definition->getSettings();
+                $pfield_name = $field_storage_definition->getName();
+                \Drupal::moduleHandler()->alter('cloned_node_paragraph_field', $value->entity, $pfield_name, $pfield_settings);
+              }
+            }
+          }
+        }
+      }
+    }
+    return $node;
   }
 
 }
